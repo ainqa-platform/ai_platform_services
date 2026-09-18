bc5cd9df15aee9115b7a4e3688988ef7a4cbcc8c
ravindraparamasivam <ravindra.paramasivam@ainqa.com>
2026-09-18 11:44:20 +0530

    added new getversion api

--- src/health.controller.ts
+++ src/health.controller.ts
@@ -1,9 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { version } from '../package.json';
 
-@Controller('api/health')
+@Controller('api')
 export class HealthController {
-  @Get()
+  @Get('health')
   check(): { status: 'ok' } {
     return { status: 'ok' };
+  }
+
+  @Get('version')
+  getVersion(): { version: string } {
+    return { version };
   }
 }
